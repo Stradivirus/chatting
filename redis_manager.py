f299401e205a921290a54a93b53362187ebdddf3
stradivirus <stradivirus9@gmail.com>
2024-08-02 16:12:35 +0900

    test4

--- redis_manager.py
+++ redis_manager.py
@@ -1,7 +1,7 @@
 import os
 import json
 import asyncio
-from aioredis import RedisCluster
+from redis.asyncio import RedisCluster
 
 class RedisManager:
     def __init__(self):
@@ -13,7 +13,8 @@ class RedisManager:
         if not self.redis:
             try:
                 startup_nodes = [{"host": host.split(':')[0], "port": int(host.split(':')[1])} for host in self.redis_hosts]
-                self.redis = await RedisCluster.from_url(f"redis://{self.redis_hosts[0]}", startup_nodes=startup_nodes)
+                self.redis = RedisCluster(startup_nodes=startup_nodes, decode_responses=True)
+                await self.redis.initialize()
                 self.pubsub = self.redis.pubsub()
             except Exception as e:
                 print(f"Failed to connect to Redis: {e}")
